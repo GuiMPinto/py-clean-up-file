593f75513705a34e98b26769e7cce3bd815d6a46
PintoJR <guimoraespinto@gmail.com>
2026-09-24 05:07:24 -0300

    Solution08

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self) -> None:
-        return
+    def __enter__(self) -> "CleanUpFile":
+        return self
 
     def __exit__(self, exc_type: type[BaseException] | None,
                  exc_val: BaseException | None,
